8f21b7e8e89881a5266cfdcb3b6c63cc47ede47c
Luis Henao <lhenao@consensussa.com>
2025-05-25 00:36:58 -0500

    Fix: email on delivery model

--- src/controller/deliveryController.js
+++ src/controller/deliveryController.js
@@ -13,7 +13,7 @@ const createDelivery = async (req, res) => {
         })
     }
     
-    let { full_name, location_id } = req.body; 
+    let { full_name, location_id, email} = req.body; 
     try {
         const location = await prisma.location.findFirst({
             where: {
@@ -30,7 +30,8 @@ const createDelivery = async (req, res) => {
         const delivery = await prisma.delivery.create({
             data: {
                 full_name, 
-                location_id
+                location_id,
+                email
             },
         });
         res.status(201).json({
