bf04ad658ea233b6105123fd50c9c2dc3a8e2839
Luis Henao <lhenao@consensussa.com>
2025-06-03 04:49:40 -0500

    Cors Policy to Gateway

--- src/index.js
+++ src/index.js
@@ -7,13 +7,6 @@ const {swaggerUi, swaggerDocs} = require("./middlewares/swagger.js")
 
 const app = express();
 const port = process.env.PORT;
-app.use(cors(
-    {
-        origin: '*', // Permitir todas las solicitudes de origen
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Métodos permitidos
-        allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-    }
-)); 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.listen(port, '0.0.0.0',() => {
