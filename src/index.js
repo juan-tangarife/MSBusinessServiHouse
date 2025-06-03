dc04b4f622eb64a17f1097b83e60ae8c10f2f097
Luis Henao <lhenao@consensussa.com>
2025-06-03 04:03:02 -0500

    Feat: Full Docker integration with ApiGateway

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(cors(
 )); 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.listen(port, () => {
+app.listen(port, '0.0.0.0',() => {
     console.log(`Project running ${port}`); 
 });
 connectDB();
